88d08c6aac0cfd2bf5b81f343ec51df7f2a199b5
seonyo <jsy6535@naver.com>
2024-08-16 11:09:44 +0900

    제네릭 실습 1

--- CSAvd/Program.cs
+++ CSAvd/Program.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace CSAvd
 {
+    class Wanted<T>
+    {
+        public T Value;
+        public Wanted(T value) {
+            this.Value = value; 
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Wanted<string> wantedString = new Wanted<string>("string");
+            Wanted<int> wantedInt = new Wanted<int>(52273);
+            Wanted<double> wantedDouble = new Wanted<double>(52.273);
+
+            Console.WriteLine(wantedString.Value);
+            Console.WriteLine(wantedInt.Value);
+            Console.WriteLine(wantedDouble.Value);
         }
     }
 }
