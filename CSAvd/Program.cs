6534c9d1277a584e037294274d36e000e40814a1
seonyo <jsy6535@naver.com>
2024-08-22 11:03:26 +0900

    구조체 실습2 - 클래스와 구조체 비교

--- CSAvd/Program.cs
+++ CSAvd/Program.cs
@@ -60,6 +60,26 @@ namespace CSAvd
                 testB = test;
             }
         }
+        class PointClass
+        {
+            public int x;
+            public int y;
+            public PointClass(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+        struct PointStruct
+        {
+            public int x;
+            public int y;
+            public PointStruct(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
 
         static void NextPosition(int x, int y, int vx, int vy,
             out int rx, out int ry)
@@ -103,9 +123,24 @@ namespace CSAvd
             int vx = 0;
             int vy = 0;
             Console.WriteLine("현재 좌표 : (" + x + ". " + y + ")");
+
             NextPosition(x, y, vx, vy, out x, out y);
             Console.WriteLine("현재 좌표 : (" + x + ". " + y + ")");
+            PointClass pCA = new PointClass(10, 20);
+            PointClass pCB = pCA;  // 참조 복사. pCA와 pCB는 동일 객체를 바라본다.
+            pCB.x = 100;
+            pCB.y = 100;
+            Console.WriteLine("pCA: {0}, {1}", pCA.x, pCA.y); // pCA: 100, 100
+            Console.WriteLine("pCB: {0}, {1}", pCB.x, pCB.y); // pCB: 100, 100
+            Console.WriteLine();
 
+            PointStruct pSA = new PointStruct(10, 20);
+            PointStruct pSB = pSA;  // 값 복사. pSA와 pSB는 별개의 변수.
+            pSB.x = 100;
+            pSB.y = 100;
+            Console.WriteLine("pSA: {0}, {1}", pSA.x, pSA.y); // pSA: 10, 20
+            Console.WriteLine("pSB: {0}, {1}", pSB.x, pSB.y); // pSB: 100, 100
+            Console.WriteLine();
         }
     }
 }
